58c8ef76f3aaf4a0e0fc82642db135419cfac4fa
integerwjo <wanderaotieno321@gmail.com>
2025-08-12 12:24:04 +0300

    complete render yaml file

--- webapi/settings.py
+++ webapi/settings.py
@@ -21,14 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v5f$=(2bia&=$vcgduj1l0x!#xsawf3hk43*4#subx_srada!t'
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback_secret_key")
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 # Application definition
 
